851f6b509f5901d6c44559b4e8052baed3003535
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-09-30 10:06:45 -0500

    contacts, font

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import styles from './Contacts.module.css'
 
 const Contacts = ({ contacts }) => {
   return (
     <>
-      <ul>
+      <ul className={styles.contactList}>
         {contacts.map((contact, i) => (
           <li key={i}>
-            {contact.name}: 
-            <a href={contact.link}>{contact.linkText}</a>
+            <strong>{contact.name}: </strong> 
+            <a href={contact.link} className={styles.contactLink}>{contact.linkText}</a>
           </li>
         ))}
       </ul>
---
+++ src/pages/_document.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@400;700&display=swap" rel="stylesheet"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
