a2e3f41abd765460673060f6d6ede4f7d2b212d5
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-09-30 18:37:27 -0500

    barebones project page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,9 +1,18 @@
 import Layout from '../../components/Layout'
+import projects from '../../data/projects.json'
 
 const Projects = (pageProps) => {
   return (
     <Layout>
-      <h1>This page is where projects will go</h1>
+      <ul>
+        {projects.map((project, i) => (
+          <li key={i}>
+            <a href={project.url}>{project.name}</a>
+            <p><strong>Description: </strong>{project.description}</p>
+            <p><strong>Technologies: </strong>{project.technologies}</p>
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 }
