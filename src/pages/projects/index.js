8fd64c56b57c02d4f8125bd298a807b920749be2
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-10-01 08:06:14 -0500

    more styling and work on projects page. added SASS/SCSS to project

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,15 +1,24 @@
 import Layout from '../../components/Layout'
 import projects from '../../data/projects.json'
+import styles from './projects.module.scss'
 
 const Projects = (pageProps) => {
   return (
     <Layout>
-      <ul>
+      <ul className={styles.List}>
         {projects.map((project, i) => (
           <li key={i}>
-            <a href={project.url}>{project.name}</a>
+            <a href={project.url} target="_blank" rel="noreferrer">
+              {project.name}
+            </a>
             <p><strong>Description: </strong>{project.description}</p>
-            <p><strong>Technologies: </strong>{project.technologies}</p>
+            <p><strong>Technologies:</strong>
+              <ul className={styles.List}>
+                {project.technologies.map((technology, j) => (
+                  <li key={j}>{technology}</li>
+                ))}
+              </ul>
+            </p>
           </li>
         ))}
       </ul>
