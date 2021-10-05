fa4c08b02859674fde6bcb9f40069d23188ca9e1
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-10-05 07:38:44 -0500

    changed all css files to scss and updated references

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,4 +1,4 @@
-import styles from './Grid.module.css'
+import styles from './Grid.module.scss'
 
 const Grid = (props) => {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../Home.module.css'
+import styles from '../Home.module.scss'
 
 import Layout from '../components/Layout'
 import Title from '../components/Title'
