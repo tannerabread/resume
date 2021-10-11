cc619f7575366a328050cd773dc7412f1c130acf
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-10-11 15:40:57 -0500

    build test

--- src/pages/api/posts/index.js
+++ src/pages/api/posts/index.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     const params = {
       TableName: process.env.TABLE_NAME
     }
-
+    
     db.scan(params, function(err, data) {
       if (err) {
         console.error('unable to scan table. Error JSON:', JSON.stringify(err, null, 2))
@@ -19,14 +19,9 @@ export default async function handler(req, res) {
         res.status(200).json(data.Items)
       }
     })
+
   } else {
     res.status(400).send({ message: "Only GET requests allowed" })
     return
-  }
-}
-
-export const config = {
-  api: {
-    externalResolver: true,
   }
 }
--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -2,12 +2,28 @@ import Layout from '../../components/Layout'
 import Title from '../../components/Title'
 import Grid from '../../components/Grid'
 import server from '../../config'
+import useSWR from 'swr'
+
+// const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const Posts = ({ posts }) => {
-  posts.map((post, i) => (
-    post.url = `/posts/${post.postId}`,
-    post.heading = post.postId
-  ))
+  // const { data, error } = useSWR(`${server}/api/posts`, fetcher)
+
+  // if (error) return <div>failed to load</div>
+  // if (!data) return <div>loading...</div>
+
+  // return (
+  //   <div>
+  //     {JSON.stringify(data)}
+  //   </div>
+  // )
+  // console.log("posts", posts)
+  // if (!posts) return <div>loading...</div>
+
+  // posts.map((post, i) => (
+  //   post.url = `/posts/${post.postId}`,
+  //   post.heading = post.postId
+  // ))
 
   return (
     <Layout>
@@ -16,7 +32,8 @@ const Posts = ({ posts }) => {
         subheading="Click here for code for this page"
         href="https://github.com/tannerabread/resume/blob/main/src/pages/posts/index.js"
       />
-      <Grid gridItems={posts} />
+      {/* <Grid gridItems={posts} /> */}
+      <div>Posts</div>
     </Layout>
   )
 }
@@ -24,13 +41,18 @@ const Posts = ({ posts }) => {
 export default Posts
 
 
-export async function getStaticProps() {
-  const res = await fetch(`${server}/api/posts`)
-  const posts = await res.json()
+// export async function getStaticProps() {
+//   const res = await fetch(`${server}/api/posts`)
+//   console.log("res", res)
+//   const posts = await res.json()
 
-  return {
-    props: {
-      posts,
-    }
-  }
-}
+//   console.log("getStaticProps", posts)
+//   if (!posts) {
+//     return { props: { posts: "posts" } }
+//   }
+//   return {
+//     props: {
+//       posts,
+//     }
+//   }
+// }
