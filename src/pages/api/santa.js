9475d8bb2c5bb55eb85f4e025eea1863657d69e9
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-11-16 10:26:52 -0600

    added test for put request

--- src/pages/api/santa.js
+++ src/pages/api/santa.js
@@ -9,7 +9,8 @@ export default function handler(req, res) {
 
   if (req.method === 'PUT') {
     // process PUT request
-
-    res.status(200)
+    const body = JSON.parse(req.body)
+    
+    res.status(200).json(body)
   }
 }
