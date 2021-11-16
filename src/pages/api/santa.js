3640c92756df18d6a1691f68694759030cbe5d0b
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-11-16 11:27:04 -0600

    believe the api is working now

--- src/pages/api/santa.js
+++ src/pages/api/santa.js
@@ -1,4 +1,6 @@
 import list from '../../data/list.json'
+import fs from 'fs'
+import path from 'path'
 
 export default function handler(req, res) {
   console.log('list', list)
@@ -9,8 +11,12 @@ export default function handler(req, res) {
 
   if (req.method === 'PUT') {
     // process PUT request
-    const body = JSON.parse(req.body)
-    
-    res.status(200).json(body)
+    const body = req.body
+    let objIndex = list.findIndex(obj => obj.id === body.id)
+    list[objIndex] = body
+    const dir = path.resolve('./src/data')
+    console.log('dir', dir)
+    fs.writeFileSync(dir + '/list.json', JSON.stringify(list, null, 2))
+    res.status(201).json(list)
   }
 }
